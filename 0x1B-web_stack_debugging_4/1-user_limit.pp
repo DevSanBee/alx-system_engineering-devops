3f5e770986da7310a23ae5f2a307945619474633
DevSanBee <sanusiolaniyan@gmail.com>
2023-09-18 11:05:07 +0000

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,13 +1,10 @@
-#Enable the user holberton to login and open files without errors
+# Set a new soft and hard value to the limit
+# of max open files for all the system
 
-# Increase hard file limit for holberton user
-exec { 'increase-hard-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton hard/s/5/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
+exec { 'change-max-open-files-hard-limit':
+  command => "/bin/sed -i /etc/security/limits.conf -e 's/hard nofile [0-9]\+/hard nofile 4000/g'"
 }
 
-#Increase soft file limit for user holberton
-exec { 'increase-soft-file-limit-for-holberton-user':
-  command => 'sed -i "/holberton soft/s/4/50000/" /etc/security/limits.conf'
-  path    => '/usr/local/bin/:/bin/'
+exec { 'change-max-open-files-soft-limit':
+  command => "/bin/sed -i /etc/security/limits.conf -e 's/soft nofile [0-9]\+/soft nofile 3000/g'"
 }
